4789bbffd41d225532e28a50be482a2dd18e7ca1
Thorsten Korpitsch <thorsten.korpitsch@gmail.com>
2021-05-18 07:30:19 +0200

    vpc + stability callable, added probably needed functions

--- src/experimental.py
+++ src/experimental.py
@@ -3,56 +3,66 @@ from PyQt5 import QtWidgets
 import sys
 
 from hierarchicalMesh import HierarchicalMesh
-from viewpointselection.app import MainWindow
-import viewpointselection.entropy
-
-anchor = HierarchicalMesh(None, None, "Anchor")
+from viewpointselection.app import ViewPointComputation
 
 
-stlReader = vtk.vtkSTLReader()
-stlReader.SetFileName("../meshes/mid_mesh.stl")
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(stlReader.GetOutputPort())
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-actor.GetProperty().SetOpacity(0.5)
-actor.GetProperty().SetColor(0, 1, 0)
+def build_hierarchy():
+    anchor = HierarchicalMesh(None, None, "Anchor")
+    stlReader = vtk.vtkSTLReader()
+    stlReader.SetFileName("../meshes/mid_mesh.stl")
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(stlReader.GetOutputPort())
+    actor1 = vtk.vtkActor()
+    actor1.SetMapper(mapper)
+    actor1.GetProperty().SetOpacity(0.5)
+    actor1.GetProperty().SetColor(0, 1, 0)
 
-anchor.add(actor, "../meshes/mid_mesh.stl")
+    anchor.add(actor1, "../meshes/mid_mesh.stl")
 
-stlReader.SetFileName("../meshes/inner_mesh.stl")
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(stlReader.GetOutputPort())
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-actor.GetProperty().SetOpacity(0.5)
-actor.GetProperty().SetColor(1, 0, 0)
+    stlReader.SetFileName("../meshes/inner_mesh.stl")
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(stlReader.GetOutputPort())
+    actor2 = vtk.vtkActor()
+    actor2.SetMapper(mapper)
+    actor2.GetProperty().SetOpacity(0.5)
+    actor2.GetProperty().SetColor(1, 0, 0)
 
-anchor.add(actor, "../meshes/inner_mesh.stl")
+    anchor.add(actor2, "../meshes/inner_mesh.stl")
 
-stlReader.SetFileName("../meshes/outer_mesh.stl")
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(stlReader.GetOutputPort())
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-actor.GetProperty().SetOpacity(0.5)
-actor.GetProperty().SetColor(0, 0, 1)
+    stlReader.SetFileName("../meshes/outer_mesh.stl")
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(stlReader.GetOutputPort())
+    actor3 = vtk.vtkActor()
+    actor3.SetMapper(mapper)
+    actor3.GetProperty().SetOpacity(0.5)
+    actor3.GetProperty().SetColor(0, 0, 1)
 
-anchor.add(actor, "../meshes/outer_mesh.stl")
+    anchor.add(actor3, "../meshes/outer_mesh.stl")
 
-stlReader.SetFileName("../meshes/inner_mesh.stl")
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(stlReader.GetOutputPort())
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-actor.GetProperty().SetOpacity(0.5)
-actor.GetProperty().SetColor(1, 1, 0)
+    stlReader.SetFileName("../meshes/inner_mesh.stl")
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(stlReader.GetOutputPort())
+    actor4 = vtk.vtkActor()
+    actor4.SetMapper(mapper)
+    actor4.GetProperty().SetOpacity(0.5)
+    actor4.GetProperty().SetColor(1, 1, 0)
 
-anchor.add(actor, "../meshes/inner_mesh.stl")
+    anchor.add(actor4, "../meshes/inner_mesh.stl")
+    return anchor
 
-print(anchor.toList())
 
 if __name__ == "__main__":
+    anchor = build_hierarchy()
+    print(anchor.toList())
     app = QtWidgets.QApplication(sys.argv)
-    ec = MainWindow(anchor.toList())
-
+    vpc = ViewPointComputation(anchor.toList())
+    app.exec_()
+    vpc.close()
+    # anchor.convexify -> anchor.simplify -> anchor.recusrive_difference
+    # for plane in vpc.planes():
+        # anchor.cut_with_plane (?)
+        # is_stable = vpc.stability([anchor.all_previously_generated_objects()], "../viewpointselection/data/urdf/plane.urdf")
+        # if is_stable:
+            # break
+    # anchor.unfold_all_previously_generated_objects()
+    print(vpc.stability(anchor.generated_by_plane_cut, "../viewpointselection/data/urdf/plane.urdf"))
