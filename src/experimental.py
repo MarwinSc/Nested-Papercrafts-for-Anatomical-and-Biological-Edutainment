3a2e52e5c17f73eb7aceb9aaf3b2bcca22220529
Thorsten <thorsten.korpitsch@gmail.com>
2021-08-24 16:58:55 +0200

    exemplary drawing of numbers for gt using mu3d files

--- src/experimental.py
+++ src/experimental.py
@@ -21,31 +21,53 @@ def read_and_render_model(path, ambient, render):
     render.AddActor(model_actor)
 
 
-def read_and_render_IDs(path, ambient, render):
+def read_and_render_IDs(path, path_mirror, render, scale=0.1):
     reader = vtk.vtkOBJReader()
     reader.SetFileName(path)
     reader.Update()
 
     polydata = reader.GetOutput()
-    faces = vtk_to_numpy(polydata.GetPolys().GetData())
     points = vtk_to_numpy(polydata.GetPoints().GetData()).astype(float)
+    indices = np.unique(points, axis=0, return_index=True)[1]
+    points = [points[index] for index in sorted(indices)]
+
+    reader.SetFileName(path_mirror)
+    reader.Update()
+
+    polydata = reader.GetOutput()
+    mirrorPoints = vtk_to_numpy(polydata.GetPoints().GetData()).astype(float)
+    indices = np.unique(mirrorPoints, axis=0, return_index=True)[1]
+    mirrorPoints = [mirrorPoints[index] for index in sorted(indices)]
 
     id = 1
-    for i in range(0, len(faces) - 2, 5):
-        txt = vtk.vtkTextActor()
-        txt.SetInput(str(id))
+    for i in range(0, len(points), 4):
+        label = vtk.vtkVectorText()
+        label.SetText(str(id))
         id += 1
-        txtprop = txt.GetTextProperty()
-        txtprop.SetFontFamilyToArial()
-        txtprop.SetFontSize(18)
-        txt.GetProperty().SetColor(ambient, 0, 0)
-        pos = (points[faces[i]] + points[faces[i + 1]] + points[faces[i + 2]] + points[faces[i + 3]] + points[
-            faces[i + 4]] + points[faces[i + 5]]) / 6
+        pos = (points[i] + points[i + 1] + points[i + 2] + points[i + 3]) / 4
 
-        print(pos)
-        txt.SetPosition(pos[0], pos[1])
-        render.AddActor(txt)
+        mirrorPos = (mirrorPoints[i] + mirrorPoints[i + 1] + mirrorPoints[i + 2] + mirrorPoints[i + 3]) / 4
 
+        lblMapper = vtk.vtkPolyDataMapper()
+        lblMapper.SetInputConnection(label.GetOutputPort())
+
+        # Set up an actor for the node label
+        lblActor = vtk.vtkFollower()
+        lblActor.SetMapper(lblMapper)
+        lblActor.SetScale(scale, scale, scale)
+        lblActor.SetPosition(pos[0] - scale / 2, pos[1] - scale / 2, 0.0)
+        lblActor.GetProperty().SetColor(255, 0, 0)
+        render.AddActor(lblActor)
+
+        lblMapper = vtk.vtkPolyDataMapper()
+        lblMapper.SetInputConnection(label.GetOutputPort())
+        # Set up an actor for the node label
+        lblActor = vtk.vtkFollower()
+        lblActor.SetMapper(lblMapper)
+        lblActor.SetScale(scale, scale, scale)
+        lblActor.SetPosition(mirrorPos[0] - scale / 2, mirrorPos[1] - scale / 2, 0.0)
+        lblActor.GetProperty().SetColor(0, 255, 0)
+        render.AddActor(lblActor)
 
 def main():
     renderer = vtk.vtkRenderer()
@@ -54,7 +76,7 @@ def main():
     read_and_render_model(r"C:\repositories\AnatomicalEdutainer\meshes\gluetabs.obj", 0.3, renderer)
     read_and_render_model(r"C:\repositories\AnatomicalEdutainer\meshes\mirrorgt.obj", 0.2, renderer)
 
-    read_and_render_IDs(r"C:\repositories\AnatomicalEdutainer\meshes\gluetabs.obj", 1.0, renderer)
+    read_and_render_IDs(r"C:\repositories\AnatomicalEdutainer\meshes\gluetabs.obj", r"C:\repositories\AnatomicalEdutainer\meshes\mirrorgt.obj", renderer)
 
     renderer.SetBackground(vtk.vtkNamedColors().GetColor3d('White'))
     window = vtk.vtkRenderWindow()
